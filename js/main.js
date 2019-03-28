3e097a3b44b555be86f19557e0e61c57aa6e2293
josandreu <jose.andreu.r@gmail.com>
2019-03-28 19:32:31 +0100

    ok

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const boton = $('#boton');
 const ciudad = $('#ciudad');
-
+const contenido = $('#contenido');
 
 // FUNCIONES
 
@@ -12,12 +12,42 @@ const llamarAPI = (miUrl) => {
         dataType: 'json',
         success: function (response) {
             console.log(response);
+            // estas variables las sacamos del objeto json que retorna la llamada ajax
+            let miContenido = '';
+            let lugar = response.name; // accedemos al nombre de la ciudad
+            let pais = response.sys.country;
+            let temperatura = response.main.temp;
+            let rutaIcono = `http://openweathermap.org/img/w/${response.weather[0].icon}.png`;
+            let descripcion = response.weather[0].description;
+            miContenido =
+                /*html*/
+                `
+                <figure class="figure">
+                    <img src="${rutaIcono}" width="150px" class="figure-img img-fluid rounded" alt="${lugar}">
+                    <figcaption class="figure-caption">
+                    <b>${lugar}</b> (${pais}) <span class="temperatura rounded ml-2">${temperatura}°</span>
+                    <p class="mt-3"><i>${descripcion}</i></p>
+                    </figcaption>
+                </figure>
+                `;
+            // document.getElementById('contenido').innerHTML = contenido;
+            contenido.html(miContenido);
 
         },
         error: function () {
-            console.log("Ha habido un error en la consulta Ajax...");
+            let miContenido =
+                /*html*/
+                `
+                <div class="bg-danger p-5 rounded">
+                    <p>Ha habido un error en la solicitud a la API</p>
+                    <p>Tal vez la localidad <b>${ciudad.val()}</b> no exista...</p>
+                </div>
+                `;
+            contenido.html(miContenido);
         }
     });
+    // limpiamos el contenido del input de la busqueda una vez realizada la busqueda
+    ciudad.val('');
 };
 
 const prepararUrl = (e) => {
@@ -26,6 +56,8 @@ const prepararUrl = (e) => {
     let c = ciudad.val();
     miUrl += 'q=' + c + ',es';
     miUrl += '&APPID=' + key;
+    miUrl += '&units=metric';
+    miUrl += '&lang=es';
 
     llamarAPI(miUrl);
 
@@ -35,10 +67,19 @@ const inicializar = () => {
 
 };
 
+const detectarTecla = (e) => {
+    console.log(e); // es el campo keyCode del dom el que nos interesa
+    console.log(e.keyCode); // tecla 13 (enter)
+    if (e.keyCode === 13) {
+        prepararUrl(); // cuando le demos al enter, llama a la función prepararUrl()
+    }
 
+};
 
 // EVENTOS
 boton.on('click', prepararUrl);
+// añadimos un listener a la barra de busqueda, actuará cuando pulsemos una tecla (queremos con la tecla enter)
+ciudad.on('keyup', detectarTecla);
 /*
 $(document).ready(function () {
     inicializar();
